1aee5f48653bed7ebe8ba85d34e6f52815044969
Santiago L. Valdarrama <svpino@gmail.com>
2024-06-24 19:32:46 +0200

    ...

--- test_train.py
+++ test_train.py
@@ -1,14 +1,17 @@
-import pytest
 from train import load_data
 
-def test_load_data_shapes():
+
+def test_load_data_loads_images():
     X_train, y_train, X_test, y_test = load_data()
+    
     assert X_train.shape == (60000, 784)
     assert y_train.shape == (60000,)
     assert X_test.shape == (10000, 784)
     assert y_test.shape == (10000,)
 
-def test_load_data_range():
+
+def test_load_data_scales_images():
     X_train, y_train, X_test, y_test = load_data()
+
     assert X_train.min() >= 0 and X_train.max() <= 1
     assert X_test.min() >= 0 and X_test.max() <= 1
