2f76039b014a089211bccd8e26a26bde4ef74586
Santiago L. Valdarrama <svpino@gmail.com>
2024-06-24 19:22:52 +0200

    ...

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # actions-sample
+
+Create a virtual environment, update pip, and install the required packages:
+
+```
+$ python3 -m venv .venv
+$ source .venv/bin/activate
+$ pip install -U pip
+$ pip install -r requirements.txt
+```
---
+++ train.py
@@ -0,0 +1,59 @@
+import os
+
+os.environ["KERAS_BACKEND"] = "tensorflow"
+
+from keras import Input, datasets
+from keras.layers import Dense
+from keras.models import Sequential
+
+
+def load_data():
+    (X_train, y_train), (X_test, y_test) = datasets.mnist.load_data()
+
+    X_train = X_train.reshape(-1, 784)
+    X_test = X_test.reshape(-1, 784)
+
+    X_train = X_train / 255.0
+    X_test = X_test / 255.0
+
+    return X_train, y_train, X_test, y_test
+
+
+def build_model():
+    """Build a simple neural network model using
+    the supplied output layer."""
+
+    model = Sequential(
+        [
+            Input(shape=(784,)),
+            Dense(512, activation="relu"),
+            Dense(256, activation="relu"),
+            Dense(10, activation="softmax"),
+        ]
+    )
+
+    return model
+
+
+def train():
+    model = build_model()
+    model.compile(
+        optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+    )
+
+    X_train, y_train, X_test, y_test = load_data()
+
+    model.fit(
+        X_train,
+        y_train,
+        batch_size=32,
+        epochs=5,
+        verbose=1,
+        validation_data=(X_test, y_test),
+    )
+
+    return model
+
+
+model = train()
+model.save("model.keras")
